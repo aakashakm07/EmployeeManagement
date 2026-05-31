f138fbd5af749e4e9ac47186740b6e924b54a343
aakashakm07 <Aakashhindsol24@gmail.com>
2026-05-31 13:37:19 +0530

    some change

--- app/dashboard/attendence/page.tsx
+++ app/dashboard/attendence/page.tsx
@@ -2,6 +2,7 @@ export default function Attendence(){
     return (
         <div>
             <h2>Showing Attendence Records</h2>
+            <h3 className="bg-green-800 text-red-950">This is the employee record management system</h3>
         </div>
 
     )
